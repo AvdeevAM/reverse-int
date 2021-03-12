5b6afb58a04b614960e3133d68dd26d494ade479
avdeevam <avdeev.am471@gmail.com>
2021-03-12 18:18:26 +0300

    feat: add reverseInt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse (num) {
+    var result = num.toString().split('').reverse().join('');
+    return parseInt(result);
+    }
